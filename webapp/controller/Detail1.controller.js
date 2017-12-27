2eb0e2f01ebecea286ad97d32c199c7ad6b00395
Rawat Subhashini <subhashini_rawat@infosys.com>
2017-12-27 10:59:39 +0000

    donotchange

--- webapp/controller/Detail1.controller.js
+++ webapp/controller/Detail1.controller.js
@@ -44,7 +44,7 @@ sap.ui.define([
 			}
 		},
 		
-		newFunctionEditedinremote : function()
+		donotchange : function()
 		{
 			this.getOwnerComponent().getRouter().getRoute("orderDetails").attachPatternMatched(this._onRouteMatched, this);
 		}
